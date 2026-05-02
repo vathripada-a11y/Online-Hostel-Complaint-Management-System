a1efbc47e38633990dd7b3a75516565353922e13
vathripada-a11y <vathripada@gmail.com>
2026-05-02 12:52:47 +0530

    add server.js with basic routes

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('Hostel Complaint System is running!')
+})
+app.get('/student', (req, res) => {
+  res.send('Welcome Student!')
+})
+
+app.get('/warden', (req, res) => {
+  res.send('Welcome Warden!')
+})
+
+app.get('/admin', (req, res) => {
+  res.send('Welcome Admin!')
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000')
+})
